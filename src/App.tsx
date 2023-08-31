e79a22f61292bbd43d830936a0b6310be008294a
Emmanuel Akinbo <59098948+akinbo-emmanuel@users.noreply.github.com>
2023-08-31 01:26:31 +0100

    started work

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './Components/Nav'
+import { useState } from "react";
+import "./App.css";
+import Nav from "./Components/Nav";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
- <Nav />
+      <Nav />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Nav:React.FC = () => {
+const Nav: React.FC = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div>
+      <div className="w-screen py-10 flex  justify-between bg-red-500 shadow-xl"></div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
